e7330352d4e625e602cc841f88565c888474d630
konok101 <rockykonok@gmail.com>
2022-02-22 03:12:31 +0600

    for loop started

---
+++ loop.js
@@ -0,0 +1,20 @@
+
+
+// array use to loop
+
+let array = ['one', 'two', 'three'];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+for (i = array.length - 1; i >= 0; i--) {
+
+    console.log(array[i])
+}
+
+
+
+
+
+
