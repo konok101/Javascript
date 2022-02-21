7e67e49979e4245b09d12857d3720bcd1871fada
konok101 <rockykonok@gmail.com>
2022-02-22 02:30:03 +0600

    function part done

---
+++ function.js
@@ -0,0 +1,88 @@
+// parameter and its evalatioN
+
+/*
+function test(a, b) {
+    console.log('hello ' + " " + a + "    " + b);
+}
+test("KONOK", "MAJAMDER");
+*/
+
+
+
+// JAKON UNDEFINE THAKBA KNO VALUE
+/*
+function add(a, b) {
+    if (typeof a === 'undefined') {
+        a = " konk";
+    }
+
+    if (typeof b === 'undefined') {
+        b = 'rocky'
+
+    }
+    console.log('hello  ' + a + " " + b);
+}
+add();
+*/
+
+
+
+
+// return valueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+/*function value(a) {
+    const c = a * a;
+    return c;
+}
+const square = value(5);
+console.log(square);
+*/
+
+
+
+
+
+// another return valueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+/* function add(a, b) {
+    if (typeof a === 'undefined') {
+        a = " konk";
+    }
+
+    if (typeof b === 'undefined') {
+        b = 'rocky'
+
+    }
+    const result = ('hello  ' + a + " " + b);
+
+    return result;
+}
+const value = add();
+console.log(value); */
+
+
+
+
+
+// Naming funtion (get, set, calc, create, check)
+/*
+getVAlue= jokon kno value pabo tokon function name amn dibo
+setVAlue= jokon set korbo -------------------
+calsqaure= jkokon root/ math korbo_________
+checkVAlue= jokon value check korbo
+
+*/
+
+
+
+
+
+// ANONIMAS FUNCTION // ja function er nam thaka na take bola hoy
+(function (a, b) {
+    const c = a + b;
+    console.log(c);
+})(21, 22);
+
+
+
+
+
